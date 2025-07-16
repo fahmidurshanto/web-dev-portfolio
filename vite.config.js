293a72d3bdc290a8b7f62ca0576227bac6b38387
Fahmidur Rahaman Shanto <fahmidurrahamanshanto@gmail.com>
2025-07-17 01:41:11 +0600

    vercel

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
-      external: ['react-scroll', 'react-parallax'],
+      external: ['react-parallax'],
     },
   },
 })
