5740a6f9ee29b9b91e40c18972ef2627ecee1a42
Fahmidur Rahaman Shanto <fahmidurrahamanshanto@gmail.com>
2025-07-17 01:36:49 +0600

    Added vercel config again

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
-      external: ['react-scroll'],
+      external: ['react-scroll', 'react-parallax'],
     },
   },
 })
