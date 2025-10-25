6fcfc54eb1ce9c5b8572c1789b0a77110c2e3629
Fahmidur Rahaman Shanto <118394676+fahmidurshanto@users.noreply.github.com>
2025-10-25 10:26:37 +0600

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -115,11 +115,7 @@ const Hero = () => {
                   }`}
                   onLoad={() => setIsLoaded(true)}
                 />
-                <div className="absolute -bottom-3 -right-3 bg-[var(--primary-color)] rounded-full p-3 shadow-lg animate-bounce">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                  </svg>
-                </div>
+                
               </div>
             </div>
           </div>
