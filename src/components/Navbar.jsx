19249459d21daad3c6ecc86b5628dc10ebb1c71a
Fahmidur Rahaman Shanto <118394676+fahmidurshanto@users.noreply.github.com>
2025-10-25 11:36:05 +0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className={`fixed w-full z-50 mx-auto transition-all duration-500 ${
+    <nav className={`fixed w-full z-50  transition-all duration-500 ${
       scrolled 
         ? 'bg-[var(--background-color)] bg-opacity-90 backdrop-blur-lg py-3 shadow-2xl' 
         : 'bg-transparent py-5'
